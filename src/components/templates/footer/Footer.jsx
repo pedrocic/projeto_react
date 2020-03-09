ac8cee01a6f5d290f4eaffd87a038caa9c2c0969
PedroCIC <pedro.unb.cic@gmail.com>
2020-03-09 12:09:22 -0300

    Footer

--- src/components/templates/footer/Footer.jsx
+++ src/components/templates/footer/Footer.jsx
@@ -3,6 +3,18 @@ import React from 'react';
 
 export default () => (
   <footer className="footer">
-    Rodapé
+    <span>
+      Desenvolvido com
+      {' '}
+      <i className="fa fa-heart text-danger" />
+      {' '}
+      por
+      {' '}
+      <strong>
+        Cod
+        <span className="text-danger">3</span>
+        r
+      </strong>
+    </span>
   </footer>
 );
